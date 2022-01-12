bdae086e8b204680dca56967a472a896464bae6d
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2022-01-13 03:05:12 +0800

    update

--- postitdaddy/main/forms.py
+++ postitdaddy/main/forms.py
@@ -8,8 +8,18 @@ class ThreadForm(ModelForm):
     class Meta:
         model = Thread
         fields = ('title', 'content', 'tags', 'image', 'community', )
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', 'type': 'text'}),
+            'content' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Text(optional)'}),
+            'community' : forms.Select(attrs={'class': 'form-control',}),
+            'tags' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Put comma in between tags'}),
+            'image' : forms.ClearableFileInput(attrs={'class': 'form-control-file', 'type': 'file', 'accept': "image/*"}),
+        }
 
 class CommunityForm(ModelForm):
     class Meta:
         model = Community
         fields = ('title',)
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Community Name', 'type': 'text'}),
+        }
